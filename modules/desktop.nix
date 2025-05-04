44504ebb9fbf601c9187deab1834e4fa16e3d8f8
vlekje513 <robbom2010@hotmail.com>
2025-05-04 23:37:33 +0200

    Update desktop.nix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,7 +4,7 @@
   services.xserver.enable = true;
 
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
     layout = "us";
