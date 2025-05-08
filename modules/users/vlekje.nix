eb2a1d4203a311a78c3b6def6c225b4209df884a
vlekje513 <robbom2010@hotmail.com>
2025-05-08 15:14:30 +0200

    fix #900

--- modules/users/vlekje.nix
+++ modules/users/vlekje.nix
@@ -18,7 +18,7 @@
       vscode
       brave
     ];
-    openssh.authorizedKeys = [
+    openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5mLMzwlx+4FkB4q3dybESRH4uKYRCqVUNkZs/YL+Nj"
     ];
   };
